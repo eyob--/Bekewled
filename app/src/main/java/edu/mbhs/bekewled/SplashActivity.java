1087a07e460f04ba2a1b53a57c25ea2ecbf6c7dd
eyob-- <tse.eyob@gmail.com>
2017-02-14 11:21:53 -0500

    Splash Screen!!!

---
+++ app/src/main/java/edu/mbhs/bekewled/SplashActivity.java
@@ -0,0 +1,20 @@
+package edu.mbhs.bekewled;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
